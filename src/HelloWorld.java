920a2973a6d86c6bf793d48e1982f559bbf0c5b4
Abrehett <seseney2022@gmail.com>
2022-01-23 16:53:53 -0500

    fixes

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,6 +1,7 @@
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("I love my JAVA");
-        System.out.println("%%$%$");
+        System.out.println("I love JAVA");
+
+
     }
      }
